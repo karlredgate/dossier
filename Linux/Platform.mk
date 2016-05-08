1d763e82e0c627e3708de92866091628e1c1c0f1
Karl Redgate <Karl.Redgate@gmail.com>
2016-05-08 11:39:05 -0400

    add platform specific makefile includes

--- Linux/Platform.mk
+++ Linux/Platform.mk
@@ -1,1 +1,8 @@
+
+DISTRO := $(shell lsb_release --short --id)
+RELEASE := $(shell lsb_release -rs)
+CODENAME := $(shell lsb_release -cs)
+
+include $(wildcard Makefiles/$(DISTRO).mk Makefiles/$(DISTRO)$(RELEASE).mk Makefiles/$(CODENAME).mk)
+
 LDSOFLAGS += -shared -Wl,-soname,$(SONAME)
