44ee177e727bccc227757c4c42ec3ca07d103394
Karl Redgate <Karl.Redgate@gmail.com>
2016-12-30 09:34:02 -0500

    updates to fedora build

--- Makefiles/Fedora.mk
+++ Makefiles/Fedora.mk
@@ -1,6 +1,7 @@
-
-$(warning Loading rules for Fedora)
+$(warning Included Fedora rules)
 
 CXXFLAGS += $(shell pkg-config --cflags tcl )
 CFLAGS += $(shell pkg-config --cflags tcl )
 LDFLAGS += $(shell pkg-config --libs tcl )
+
+include Makefiles/RPM.mk
